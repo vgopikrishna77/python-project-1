610c44dd43c9b74680d174cbee8e8a98211cf045
nagamanideepika <nagineninagamanideepika@gmail.com>
2021-11-19 13:11:37 +0530

    main.py,main1.py are saple scrits

---
+++ main.py
@@ -0,0 +1,5 @@
+n=input()
+if n==s[::-1]:
+	print(""palindrome"")
+else:
+	print("not palindrome")
---
+++ main1.py
@@ -0,0 +1,7 @@
+from math import *
+s=int(input())
+n=math.sqrt(s)
+if n*n==s:
+ print("perfect sqare")
+else:
+ print("not perfect sqare")
